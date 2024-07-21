f76a91a811e78051240dff9c6e6bdb070f83e990
MarcosFree3D <marcos@somoscreativos.org>
2024-07-21 02:46:11 +0200

    Create plantilla base and add fonts

---
+++ resources/views/components/plantilla-base.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blendiberia</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/blendiberia.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/registro.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/talleres.css') }}">
+</head>
+<body>
+
+    <x-main-menu/>
+    {{$slot}}
+    <!-- CUERPO -->
+    <x-cabecera-principal/>
+    <x-calendario-localizacion/>
+    <x-zona-ponentes/>
+    <x-zona-conferencias/>
+    <x-zona-talleres/>
+
+    <x-boton-reserva-naranja/>
+
+    <x-zona-videos/>
+
+    <!-- PIE DE PAGINA -->
+    <footer class="b-pie-pagina">
+        <div></div>
+        <div class="b-imagenlogopie">
+            <img class="imagenlogopie" src={{ asset('/images/svg/logo.svg') }} alt="Logo Blendiberia" >
+        </div>
+        <div>
+            <p class="textolegal"> Aviso legal </p>
+            <p class="textolegal"> <a href="/politica-privacidad">Politica de privacidad</a> </p>
+            <p class="textolegal"> Politica de cookies </p>
+        </div>
+    </footer>
+    <script src={{ asset('/javascript/blendiberia.js') }}></script>
+    <script src={{ asset('/javascript/dark.js')}}></script>
+
+</body>
+</html>
+
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,37 +1,13 @@
-<header>
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/blendiberia.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/registro.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/dark.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/talleres.css') }}">
-</header>
-<x-main-menu/>
+<x-plantilla-base>
+    <x-cabecera-principal/>
+    <x-calendario-localizacion/>
+    <x-zona-ponentes/>
+    <x-zona-conferencias/>
+    <x-zona-talleres/>
 
-<!-- CUERPO -->
-<x-cabecera-principal/>
-<x-calendario-localizacion/>
-<x-zona-ponentes/>
-<x-zona-conferencias/>
-<x-zona-talleres/>
+    <x-boton-reserva-naranja/>
 
-<x-boton-reserva-naranja/>
+    <x-zona-videos/>
 
-<x-zona-videos/>
-
-<!-- PIE DE PAGINA -->
-<footer class="b-pie-pagina">
-    <div></div>
-    <div class="b-imagenlogopie">
-        <img class="imagenlogopie" src={{ asset('/images/svg/logo.svg') }} alt="Logo Blendiberia" >
-    </div>
-    <div>
-        <p class="textolegal"> Aviso legal </p>
-        <p class="textolegal"> <a href="/politica-privacidad">Politica de privacidad</a> </p>
-        <p class="textolegal"> Politica de cookies </p>
-    </div>
-</footer>
-<script src={{ asset('/js/blendiberia.js') }}></script>
-<script src={{ asset('/js/dark.js')}}></script>
+</x-plantilla-base>
 
