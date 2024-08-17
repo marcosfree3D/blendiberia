446bc497814d45cd7a78825e784a4dfcb945aab2
MarcosFree3D <marcos@somoscreativos.org>
2024-08-17 13:13:39 +0200

    Main menu for mobile

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -1,9 +1,9 @@
 <a class="logobarra" href="/">
     <img src="/images/svg/icono_home.svg" alt="" width="30">
 </a>
-
+ <!-- BARRA DE MENÚ escritorio -->
 <nav class="barramenu" id="id_barramenu">
-    <!-- BARRA DE MENÚ RESPONSIVE -->
+
     <div class="mobile-nav-toogle" id="mobileNavToogle">
         <img  class="hamburger" id="id_hamburger" src="/images/svg/hamburger.svg" alt=" hamburger main menu">
         <img  class="cerrar" id="id_close" src="/images/svg/close.png" alt=" close main menu">
@@ -22,6 +22,38 @@
 
         <div class="alinearderecha">
             <ul class="social">
+                <li><a  href="https://www.instagram.com/blendiberia?igsh=MW9lejVzYmc5YXMzNA=="> <img class="social_svg" src="/images/svg/instagram.svg" alt="instagram"></a></li>
+                <li><a  href="https://www.facebook.com/blendiberia"> <img class="social_svg" src="/images/svg/facebook.svg" alt="Facebook"></a></li>
+                <li><a  href="https://www.tiktok.com/blendiberia"> <img class="social_svg" src="/images/svg/tiktok.svg" alt="tiktok"></a></li>
+                <li><a  href="https://www.twitter.com/blendiberia"> <img class="social_svg" src="/images/svg/twitter.svg" alt="twitter"></a></li>
+                <li><a  href="/card/"> <img class="social_svg shopping" src="/images/svg/Icon feather-shopping-cart.svg" alt="Blendiberia Store"></a></li>
+
+                <li><a href="#"><img src="/images/svg/night-mode.svg" alt="Light/dark mode" class="switcher__btn social_svg"></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<nav class="barramenu-movil" id="id-barramenu-mobil">
+    <!-- BARRA DE MENÚ RESPONSIVE -->
+    <div class="mobile-nav-toogle" id="mobileNavToogle">
+        <img  class="hamburger" id="id_hamburger" src="/images/svg/hamburger.svg" alt=" hamburger main menu">
+        <img  class="cerrar" id="id_close" src="/images/svg/close.png" alt=" close main menu">
+    </div>
+    <div class="textobarra">
+        <ul class="links-movil">
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/ponencias/9">Ponencias</a></li>
+            <li><a href="/talleres/7">Talleres</a></li>
+            <!--<li><a href="/videos">Vídeos</a></li> -->
+            <li><a href="/merchandise">Merchandise</a></li>
+            <li><a href="/registro">Regístrate</a></li>
+           <!-- <li><a href="/faq">FAQ</a></li>
+            <li><a href="/contacto">Contacto</a></li> -->
+        </ul>
+
+        <div class="alinearderecha">
+            <ul class="social-movil">
                 <li><a  href="https://www.instagram.com/blendiberia?igsh=MW9lejVzYmc5YXMzNA=="> <img class="social_svg" src="/images/svg/instagram.svg" alt="instagram"></a></li>
                 <li><a  href="https://www.facebook.com/blendiberia"> <img class="social_svg" src="/images/svg/facebook.svg" alt="Facebook"></a></li>
                 <li><a  href="https://www.tiktok.com/blendiberia"> <img class="social_svg" src="/images/svg/tiktok.svg" alt="tiktok"></a></li>
