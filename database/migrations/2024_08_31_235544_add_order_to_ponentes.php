562d64a90e5dedf2116d3fe5fa5857531d3426ab
MarcosFree3D <marcos@somoscreativos.org>
2024-09-01 02:23:04 +0200

    order by orden

--- database/migrations/2024_08_31_235544_add_order_to_ponentes.php
+++ database/migrations/2024_08_31_235544_add_order_to_ponentes.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('ponentes', function (Blueprint $table) {
-            $table->integer('order');
+            $table->integer('orden');
         });
     }
 
