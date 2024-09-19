b4b6e2a8399fecb49f888ecfa60430a533cb5bf4
MarcosFree3D <marcos@somoscreativos.org>
2024-09-19 11:50:42 +0200

    Qrcode

--- resources/views/components/qr.blade.php
+++ resources/views/components/qr.blade.php
@@ -1,4 +1,4 @@
 <div class="b-calendario-localizacion-qr">
-    <img src="/images/QR/qrcode.jpeg" alt="QR code para donaciones">
+    <img src="/images/QR/qrcode.jpg" alt="QR code para donaciones">
     <p class="b-calendario-localizacion-qr__texto">Ayudanos a financiar la Blendiberia</p>
 </div>
