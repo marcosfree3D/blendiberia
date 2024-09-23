6dfec776a2b187ddafe3ced17a83bfe8599a5321
MarcosFree3D <marcos@somoscreativos.org>
2024-09-24 01:50:29 +0200

    Quit QR and add fields for pay by paypal or visa

--- resources/views/components/qr.blade.php
+++ resources/views/components/qr.blade.php
@@ -1,10 +1,5 @@
 <div class="b-calendario-localizacion-qr">
-    <a href="https://www.paypal.com/qrcodes/managed/b3a70327-f5cd-49e7-87a3-5bb6155cea3b?utm_campaign=INITIAL_LAUNCH&utm_source=PPCP_ACTION_DOWNLOAD_SAVE">
-        <img src="/images/QR/qrcode.jpg" alt="QR code para donaciones">
-        <p class="b-calendario-localizacion-qr__texto">Escanea el código y ayudanos a financiar la Blendiberia</p>
-    </a>
-
-    <script src="https://www.paypal.com/sdk/js?client-id=BAAlrHv9SbheP9Py_we0-BpM9oJ3QQeAJ-RQu4EkwXBRNoTRX52a5PnLNx0G0KvKgs6Mo6lr9c0xQTPBGk&components=hosted-buttons&disable-funding=venmo&currency=EUR"></script>
+       <script src="https://www.paypal.com/sdk/js?client-id=BAAlrHv9SbheP9Py_we0-BpM9oJ3QQeAJ-RQu4EkwXBRNoTRX52a5PnLNx0G0KvKgs6Mo6lr9c0xQTPBGk&components=hosted-buttons&disable-funding=venmo&currency=EUR"></script>
     <div id="paypal-container-RAN8VM7A9UDA6"></div>
     <script>
       paypal.HostedButtons({
