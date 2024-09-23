d89cbc82ecb0b2fa48d87b91bac178f77e1c9d08
MarcosFree3D <marcos@somoscreativos.org>
2024-09-24 01:26:18 +0200

    Update qr.blade.php

--- resources/views/components/qr.blade.php
+++ resources/views/components/qr.blade.php
@@ -3,4 +3,12 @@
         <img src="/images/QR/qrcode.jpg" alt="QR code para donaciones">
         <p class="b-calendario-localizacion-qr__texto">Escanea el código y ayudanos a financiar la Blendiberia</p>
     </a>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=BAAlrHv9SbheP9Py_we0-BpM9oJ3QQeAJ-RQu4EkwXBRNoTRX52a5PnLNx0G0KvKgs6Mo6lr9c0xQTPBGk&components=hosted-buttons&disable-funding=venmo&currency=EUR"></script>
+    <div id="paypal-container-RAN8VM7A9UDA6"></div>
+    <script>
+      paypal.HostedButtons({
+        hostedButtonId: "RAN8VM7A9UDA6",
+      }).render("#paypal-container-RAN8VM7A9UDA6")
+    </script>
 </div>
