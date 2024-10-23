72aaac7677ce9433d2b0c88ed5d9ecccb890e37b
MarcosFree3D <marcos@somoscreativos.org>
2024-10-24 01:09:47 +0200

    update taller.blade.php

--- resources/views/taller.blade.php
+++ resources/views/taller.blade.php
@@ -36,11 +36,6 @@
                         <div class="taller__resumen--precio-entrada">
                             {{$tall->precio}}€
                         </div>
-                        <!--<div class="ponencia__resumen--container-reserva">
-                            <a href="https://docs.google.com/forms/d/1PBRRTn_3f7OK6hLVlZyQ7hBDzHzNju3_AI96zpUeY3I/viewform?edit_requested=true"><div class="ponencia__resumen--boton-reserva">
-                                Reserva aquí tu entrada
-                            </div></a>
-                    </div> -->
                 @endif
             </section>
         @endforeach
